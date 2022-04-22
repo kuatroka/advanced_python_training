78e9d521de9b2ee3609dfc9e72cfc3638e2dfc1f
kuatroka <aaarmani_2000@yahoo.com>
2022-04-22 22:05:33 +0100

    .

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 Youtube  - https://www.youtube.com/watch?v=HGOBQPFzWKo&t=1002s
 
+
+## OOP Futurelearn Course
+
+https://www.futurelearn.com/courses/object-oriented-principles/18/steps/1359703
+
+
---
+++ oop_futurelearn_course/turtlerace.py
@@ -0,0 +1,55 @@
+# from turtle import Turtle
+# import turtle
+
+# # import tkinter as tk
+
+# laura = Turtle()
+
+# laura.color("red")
+# laura.shape("turtle")
+
+# laura.penup()
+# laura.goto(-200, 100)
+# laura.pendown()
+
+# rik = Turtle()
+# rik.color("blue")
+# rik.shape("turtle")
+
+# lauren = Turtle()
+# lauren.color("green")
+# lauren.shape("turtle")
+
+# carrieanne = Turtle()
+# carrieanne.color("yellow")
+# carrieanne.shape("turtle")
+
+
+# rik.goto(-160, 70)
+# lauren.goto(-160, 40)
+# carrieanne.goto(-160, 10)
+
+
+# from random import randint
+
+# for movement in range(100):
+#     laura.forward(randint(1, 10))
+#     rik.forward(randint(1, 10))
+#     lauren.forward(randint(1, 10))
+#     carrieanne.forward(randint(1, 10))
+
+# input("Press enter to exit")
+# turtle.done()
+
+###
+def tell(intro, punchline):
+    print("knock knock")
+    print("who's there?")
+    print(intro)
+    print(intro + " who")
+    print(punchline)
+
+
+tell("Atch", "Sounds like you've got a cold")
+
+###
